28608045d29935e051983e7a605cb0c59f2fb18e
stxf1993 <670356757@qq.com>
2020-01-04 20:13:36 +0800

    修改DependsOn 新增属性 @Test(dependsOnGroups = "xxx",ignoreMissingDependencies = true)

--- src/test/java/DenpendOnMethodTest.java
+++ src/test/java/DenpendOnMethodTest.java
@@ -7,9 +7,17 @@ public class DenpendOnMethodTest {
     @Test
     void login(){
         System.out.println("登陆成功");
+        //Assert.fail();
     }
     @Test(dependsOnMethods = "login")
     void getOrder(){
+        System.out.println("下单");
+    }
+
+  /*  ignoreMissingDependencies属性
+    如果为true，找不到依赖也继续执行。*/
+    @Test(dependsOnGroups = "xxx",ignoreMissingDependencies = true)
+    void getOrder3(){
         System.out.println("下单");
     }
 }
